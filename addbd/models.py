462e3649c7f144109c0fd7b991078a52fb83c058
Name <email@example.ru>
2024-02-27 13:34:31 +0700

    start sprint

---
+++ addbd/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+new = 'NW'
+pending = 'PG'
+accepted = 'AD'
+rejected = 'RD'
+
+STATUS = [
+    (new, 'новая запись'),
+    (pending, 'модератор взял в работу'),
+    (accepted,  'модерация прошла успешно'),
+    (rejected, 'модерация прошла, информация не принята'),
+]
+
+class Users(models.Model):
+    email = models.CharField(max_length=10000, unique=True)
+    fam = models.CharField(max_length=10000)
+    name = models.CharField(max_length=10000)
+    otc = models.CharField(max_length=10000)
+    phone = models.CharField(max_length=10000)
+
+
+class Coordinates(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    height = models.IntegerField()
+
+
+class Passes(models.Model):
+
+    new = 'NW'
+    pending = 'PG'
+    accepted = 'AD'
+    rejected = 'RD'
+
+    beautyTitle = models.CharField(max_length=10000)
+    title = models.CharField(max_length=10000)
+    other_titles = models.CharField(max_length=10000)
+    connect = models.CharField(max_length=10000)
+    add_time = models.DateTimeField()
+    coordinates = models.ForeignKey(Coordinates, on_delete=models.CASCADE)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    level_winter = models.CharField(max_length=10000)
+    level_summer = models.CharField(max_length=10000)
+    level_autumn = models.CharField(max_length=10000)
+    level_spring = models.CharField(max_length=10000)
+    status = models.CharField(max_length=256, choices=STATUS, )
+
+
+class Images(models.Model):
+    name = models.CharField(max_length=10000)
+    passes = models.ForeignKey(Passes, on_delete=models.CASCADE)
+
