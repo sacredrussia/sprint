2f3976954a1c3ac5f478e8aadc66db5d47a898b2
Name <email@example.ru>
2024-03-03 11:36:52 +0700

    add one more class

--- addbd/views.py
+++ addbd/views.py
@@ -91,16 +91,6 @@ class PassesAPIView(APIView):
         coords.height = set_coords['height']
         coords.save()
 
-
-        # user = Users.objects.get(pk=inst.user_id)
-        # user_update = user.objects.update(
-        #     email=set_user['email'],
-        #     fam=set_user['fam'],
-        #     name=set_user['name'],
-        #     otc=set_user['otc'],
-        #     phone=set_user['phone'],
-        # )
-
         inst.beauty_title = request.data['beauty_title']
         inst.title = request.data['title']
         inst.other_titles = request.data['other_titles']
@@ -134,13 +124,71 @@ class EmailAPIView(APIView):
         except:
             return Response({0: 'Object does not exists'})
         user_list = inst
-        passes_list = Passes.objects.get(user_id=inst.pk)
-        coord_list = Coordinates.objects.get(pk=passes_list.coordinates_id)
-        image_list = Images.objects.all()
-        image_list_filter = image_list.filter(passes_id=passes_list.pk).order_by('pk')
-        return Response({'passes': model_to_dict(passes_list),
+        passes = Passes.objects.all()
+        passes_list = passes.filter(user_id=inst.pk)
+        set_passes = []
+        set_images = []
+        set_coords = []
+        for j in passes_list:
+            e = model_to_dict(j)
+            print(e)
+            set_passes.append(e)
+            print(set_passes)
+            coords = Coordinates.objects.all()
+            coord_list = coords.filter(pk=j.coordinates_id).order_by('pk')
+            for f in coord_list:
+                h = model_to_dict(f)
+                set_coords.append(h)
+            image_list = Images.objects.all()
+            image_list_filter = image_list.filter(passes_id=j.pk)
+            for i in image_list_filter:
+                c = model_to_dict(i)
+                set_images.append(c)
+
+        return Response({'passes': set_passes,
                          'users': model_to_dict(user_list),
-                         'coordinates': model_to_dict(coord_list),
-                         'image1': model_to_dict(image_list_filter[0]),
-                         'image2': model_to_dict(image_list_filter[1])})
+                         'coordinates': set_coords,
+                         'images': set_images})
+
+    def post(self, request, *args, **kwargs):
+        email = kwargs.get('email', None)
+        if not email:
+            return Response({'error': 'Method POST not allowed'})
+        try:
+            inst = Users.objects.get(email=email)
+        except:
+            return Response({0: 'Object does not exists'})
+
+        set_coords = request.data['coords']
+        set_level = request.data['level']
+        list_image = request.data['images']
+
+        coords_new = Coordinates.objects.create(
+            latitude=set_coords['latitude'],
+            longitude=set_coords['longitude'],
+            height=set_coords['height']
+        )
+
+        passes_new = Passes.objects.create(
+            beauty_title=request.data['beauty_title'],
+            title=request.data['title'],
+            other_titles=request.data['other_titles'],
+            connect=request.data['connect'],
+            add_time=request.data['add_time'],
+            level_winter=set_level['winter'],
+            level_summer=set_level['summer'],
+            level_autumn=set_level['autumn'],
+            level_spring=set_level['spring'],
+            status='new',
+            coordinates_id=coords_new.pk,
+            user_id=inst.pk
+        )
+        for set_image in list_image:
+            image_new = Images.objects.create(
+                name=set_image['title'],
+                passes_id=passes_new.pk
+            )
+
+        return Response({'posts': 200})
+
 
